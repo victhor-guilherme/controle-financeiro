87a0d45d38d2f3818122c0c0fbe1c8d058cabe92
Victhor Guilherme <gvicthor523@gmail.com>
2026-09-21 15:51:42 -0300

    feat: adiciona cadastro e listagem de categorias

--- src/main/java/com/victhorguilherme/finance_control/categoria/Categoria.java
+++ src/main/java/com/victhorguilherme/finance_control/categoria/Categoria.java
@@ -1,4 +1,20 @@
 package com.victhorguilherme.finance_control.categoria;
 
 public class Categoria {
+
+    private long id;
+    private String nome;
+
+    public Categoria(long id, String nome){
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public long getId() {
+        return id;
+    }
 }
