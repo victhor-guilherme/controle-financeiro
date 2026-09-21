790a91dc2a8120a9fe3a29145f3a1d02572ca1ae
Victhor Guilherme <gvicthor523@gmail.com>
2026-09-21 15:21:15 -0300

    fix: impede nomes duplicados de contas

---
+++ src/main/java/com/victhorguilherme/finance_control/exceptions/NomeContaDuplicadoException.java
@@ -0,0 +1,8 @@
+package com.victhorguilherme.finance_control.exceptions;
+
+public class NomeContaDuplicadoException extends RuntimeException{
+
+    public NomeContaDuplicadoException(String message) {
+        super(message);
+    }
+}
