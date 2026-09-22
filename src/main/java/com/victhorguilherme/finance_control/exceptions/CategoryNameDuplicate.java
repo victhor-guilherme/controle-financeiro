2f0e17d2cac139dd6af07a4676499530542aa4fd
Victhor Guilherme <gvicthor523@gmail.com>
2026-09-21 21:10:38 -0300

    fix: atualização da exceção de nomes duplicados em categorias

---
+++ src/main/java/com/victhorguilherme/finance_control/exceptions/CategoryNameDuplicate.java
@@ -0,0 +1,8 @@
+package com.victhorguilherme.finance_control.exceptions;
+
+public class CategoryNameDuplicate extends RuntimeException {
+
+    public CategoryNameDuplicate(String message) {
+        super(message);
+    }
+}
