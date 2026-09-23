e545905ca69f54309a3706fcc90d8676b651e12b
Victhor Guilherme <gvicthor523@gmail.com>
2026-09-22 22:02:46 -0300

    feat: adiciona DTO de cadastro com validações

--- src/main/java/com/victhorguilherme/finance_control/auth/dto/RegisterRequest.java
+++ src/main/java/com/victhorguilherme/finance_control/auth/dto/RegisterRequest.java
@@ -1,4 +1,48 @@
 package com.victhorguilherme.finance_control.auth.dto;
 
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class RegisterRequest {
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 8)
+    private String senha;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
+
+
+
